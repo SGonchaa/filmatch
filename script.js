3b78d41f3a0beb6da286059ac1d7dec4ef7a590d
sadigonca <qoncesadiqli.0@gmail.com>
2021-10-04 16:22:48 +0400

    new

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 let swiper = new Swiper (".mySwiper", {
-    slidesPerview: 1.2,
+    slidesPerview: 1,
     spaceBetween :20,
+});
+
+
+const chevron = document.querySelector(".chev");
+const paragraph = document.querySelector(".p-paragraph");
+
+chevron.addEventListener("click", () => {
+  paragraph.classList.toggle("show");
+});
+
+let swiper = new Swiper(".mySwiper", {
+  slidesperView: 1,
+  spaceBetween: 20,
+  grabCursor: true,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+
+    1280: {
+      slidesPerView: 3,
+    },
+  },
 });
